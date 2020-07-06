b7c48b932bb68475f6f0c0fc64f17eb3f928034a
dmhliu <dmhliu@gmail.com>
2020-07-06 15:10:13 -0700

    dowload url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'daveliu',                   # Type in your name
   author_email = 'dmhliu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/dmhliu/lambdata',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/dmhliu/lambdata/archive/v_01.tar.gz',    # I explain this later on
   keywords = [ 'data wrangling', 'preprocessing'],   # Keywords that define your package best
   install_requires=[            
           'pandas',
