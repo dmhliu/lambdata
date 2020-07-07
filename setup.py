358f3170944acf639cd9763a0454ae6589e603d8
dmhliu <dmhliu@gmail.com>
2020-07-06 17:13:39 -0700

    fixed bug - setup.py missing import setuptools

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
   version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'some useful fun',   # Give a short description about your library
+  long_description= 'A collection of data science tools',
   author = 'daveliu',                   # Type in your name
   author_email = 'dmhliu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/dmhliu/lambdata',   # Provide either the link to your github or to your website
